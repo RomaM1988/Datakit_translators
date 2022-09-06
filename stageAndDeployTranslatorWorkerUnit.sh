809226d74c075c7ae34f41e9fd80691d5ece1ba3
RomaM1988 <105491461+RomaM1988@users.noreply.github.com>
2022-09-06 22:16:39 +0530

    Add files via upload

---
+++ stageAndDeployTranslatorWorkerUnit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 4 ]
+then
+        echo "stageAndDeployTranslatorWorkerUnit.sh called with incorrect number of arguments."
+        echo "stageAndDeployTranslatorWorkerUnit.sh <Interop_downloadPath> <StageDir> <ArtifactsDir> <DeployFlag>"
+        echo "For example; stageAndDeployTranslatorWorkerUnit.sh J:\datakit_translator\Baseline_Interop10 /plm/pnnas/ppic/users/<stage_dir> <ArtifactsDir> true/false"
+        exit 1
+fi
+
+Interop_Release=$1
+STAGE_BASE_DIR=$2
+ARTIFACTS_DIR=$3
+EXECUTE_DEPLOY=$4
+
+while IFS= read -r line; do
+    echo "Text read from file: $line"
+done < ${Interop_Release}/latestBaseline.txt
+
+#UNIT_PATH=
+
+# Run customer specific stage script to stage artifacts
+#chmod 0755 ${ARTIFACTS_DIR}/stage_and_deploy_artifacts.sh || { exit 1;}
+#${ARTIFACTS_DIR}/stage_and_deploy_artifacts.sh ${UNIT_PATH} ${STAGE_BASE_DIR} ${ARTIFACTS_DIR} ${EXECUTE_DEPLOY} || { exit 1;}
+
+
+
