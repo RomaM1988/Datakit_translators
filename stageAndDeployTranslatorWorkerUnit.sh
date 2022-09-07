cb1db1023f5ccc877d0249286e0beff770902d73
RomaM1988 <105491461+RomaM1988@users.noreply.github.com>
2022-09-07 14:33:56 +0530

    Update stageAndDeployTranslatorWorkerUnit.sh

--- stageAndDeployTranslatorWorkerUnit.sh
+++ stageAndDeployTranslatorWorkerUnit.sh
@@ -13,17 +13,12 @@ STAGE_BASE_DIR=$2
 ARTIFACTS_DIR=$3
 EXECUTE_DEPLOY=$4
 
-UNIT_PATH="${Interop_Release}/"
 filename="${Interop_Release}/latestBaseline.txt"
-
 line=$(head -n 1 $filename)
-echo "yes $line"
 arrIN=(${line//,/ })
-echo ${arrIN[0]}
-echo "here i "
 UNIT_PATH="${Interop_Release}/${arrIN[0]}/${arrIN[0]}"
-echo "unit path is $UNIT_PATH"
 
-# Run customer specific stage script to stage artifacts
+
+#Run customer specific stage script to stage artifacts
 #chmod 0755 ${ARTIFACTS_DIR}/stage_and_deploy_artifacts.sh || { exit 1;}
 #${ARTIFACTS_DIR}/stage_and_deploy_artifacts.sh ${UNIT_PATH} ${STAGE_BASE_DIR} ${ARTIFACTS_DIR} ${EXECUTE_DEPLOY} || { exit 1;}
