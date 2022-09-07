cec6b295967a8d09ae945351e04571b7953af3ab
RomaM1988 <105491461+RomaM1988@users.noreply.github.com>
2022-09-07 16:31:21 +0530

    Update stage_and_deploy_artifacts.sh

--- Artifacts/stage_and_deploy_artifacts.sh
+++ Artifacts/stage_and_deploy_artifacts.sh
@@ -19,12 +19,14 @@ STAGE_DIR=${STAGE_BASE_DIR}/Solidworks/lnx64/TranslatorBinaries/
 SOURCE_PATH=${UNIT_PATH}/lnx64/kits/jt_sw
 SOLIDWORKS_ARTIFACTS_DIR=${CUSTOMER_ARTIFACTS_DIR}/Solidworks/lnx64
 SOLIDWORKS_STAGE_DIR=${STAGE_DIR}jt_sw
+
 if [ ! -d ${STAGE_DIR} ]
 then
 	echo "Creating staging directory ${STAGE_DIR}"
 	mkdir -p ${STAGE_DIR} || { exit 1;}
 	chmod -R 0777 ${STAGE_DIR} || { exit 1;}
 fi
+
 if [ ! -d ${SOLIDWORKS_STAGE_DIR} ]
 then
 	echo "Creating staging directory ${SOLIDWORKS_STAGE_DIR}"
@@ -34,11 +36,6 @@ fi
 
 # Copy all 
 cp -r ${SOURCE_PATH}/*   ${SOLIDWORKS_STAGE_DIR} || { exit 1;}
-
-# Then remove selected iteams
-#rm -rf ${STAGE_DIR}/debug || { exit 1;}
-#rm -rf ${STAGE_DIR}/license || { exit 1;}
-#rm -rf ${STAGE_DIR}/dockerfile || { exit 1;}
 
 RUN_SWTOJT=${STAGE_DIR}/run_swtojt
 RUN_SWTOJT_MULTICAD=${STAGE_DIR}/run_swtojt_multicad
@@ -52,21 +49,15 @@ chmod 0755 ${RUN_SWTOJT_MULTICAD} || { exit 1;}
 
 if [ ${EXECUTE_DEPLOY} == "true" ]
 then
-	# INIT_DEF_FILE=${UNIT_PATH}/init.def
-	# stringarray=(`grep DMS_PARENT_BASELINE ${INIT_DEF_FILE} || { exit 1;}`)
-	# RELEASE_IP=${stringarray[1]}
-	# echo "Deploy flag is set to true. Executing deploy step with release IP =${RELEASE_IP}..."
-	
-	# orig=${RELEASE_IP}
-	# releaseName=${orig//'.'/'_TranslatorWorker.'}
-	
-	# cd ${STAGE_BASE_DIR} || { exit 1;}
-	# tar -czf $releaseName.tar.gz TranslatorBinaries/ || { exit 1;}
+	echo "Deploy flag is set to true. Executing deploy step for solidworks Linux build..."
+	releaseName="SWJT_LINUX"
+	cd ${STAGE_BASE_DIR}/Solidworks/lnx64 || { exit 1;}
+	tar -czf $releaseName.tar.gz TranslatorBinaries/ || { exit 1;}
 	# echo "curl -u opentools_bot:YL6MtwZ35 -T $releaseName.tar.gz https://artifacts.industrysoftware.automation.siemens.com/artifactory/generic-local/Opentools/PREVIEW/NXtoJT/$releaseName/ || { exit 1;}"
 
 	# echo "curl -u opentools_bot:YL6MtwZ35 -T NXJT_Translator_README.txt https://artifacts.industrysoftware.automation.siemens.com/artifactory/generic-local/Opentools/PREVIEW/NXtoJT/$releaseName/ || { exit 1;}"
 
-	# cd -
+	cd -
 	echo "Deploy flag is set to true. deploy step..."
 else
 	echo "Deploy flag is set to false. Skipping deploy step..."
