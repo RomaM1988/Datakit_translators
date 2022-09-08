506c9e19e42998704368d7be7240db44dfa1b2ab
RomaM1988 <105491461+RomaM1988@users.noreply.github.com>
2022-09-08 19:21:05 +0530

    Update stageAndDeployTranslatorWorkerUnit.sh

--- stageAndDeployTranslatorWorkerUnit.sh
+++ stageAndDeployTranslatorWorkerUnit.sh
@@ -19,6 +19,6 @@ arrIN=(${line//,/ })
 UNIT_PATH="${Interop_Release}/${arrIN[0]}/${arrIN[0]}"
 echo "unit path is $UNIT_PATH"
 
-#Run customer specific stage script to stage artifacts
-chmod 0755 ${ARTIFACTS_DIR}/stage_and_deploy_artifacts.sh || { exit 1;}
-${ARTIFACTS_DIR}/stage_and_deploy_artifacts.sh ${UNIT_PATH} ${STAGE_BASE_DIR} ${ARTIFACTS_DIR} ${EXECUTE_DEPLOY} || { exit 1;}
+#Run Solidworks Staging and deploy
+chmod 0755 ${ARTIFACTS_DIR}/Solidworks/solidworks_stage_and_deploy_artifacts.sh || { exit 1;}
+${ARTIFACTS_DIR}/Solidworks/solidworks_stage_and_deploy_artifacts.sh ${UNIT_PATH} ${STAGE_BASE_DIR} ${ARTIFACTS_DIR} ${EXECUTE_DEPLOY} || { exit 1;}
